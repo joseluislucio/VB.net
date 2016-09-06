e324437acd7cb327b78be1bde5a634f19ed1d528
Pepo <joseluis.lucio@gmail.com>
2016-09-06 15:14:10 +0200

    Before getItem function

--- Server Editor Qviart Combo Automator/Server Editor Qviart Combo Automator/Form1.vb
+++ Server Editor Qviart Combo Automator/Server Editor Qviart Combo Automator/Form1.vb
@@ -6,35 +6,29 @@ Public Class Form1
         Dim server, port, user, password, filename As String
         Dim listOfServers As List(Of String) = New List(Of String)
 
-        currentPosition = 1
-        '        filename = "C:\Users\lucijose\Dropbox\Qviart\" & Date.Today.Year & Date.Today.Month & Date.Today.Day
         filename = "C:\Users\lucijose\Dropbox\Qviart\" & Now.ToString("yyyyMMddHHmmss", CultureInfo.InvariantCulture)
-
-
-
-        Dim cccam As String = New System.Net.WebClient().DownloadString("https://docs.google.com/document/d/1CiYpWvLGyro-lXRHABpFC1jgD4XeACmNhas6UTSH3AQ")
-
-        currentPosition = InStr(1, cccam, "\nC: ")
+        Dim rawData As String = New System.Net.WebClient().DownloadString("https://docs.google.com/document/d/1CiYpWvLGyro-lXRHABpFC1jgD4XeACmNhas6UTSH3AQ")
+        currentPosition = InStr(1, rawData, "\nC: ") ' Find first server inside downloaded raw data
         For index As Integer = 1 To 9
-            firstSpace = InStr(currentPosition, cccam, " ")
+            firstSpace = InStr(currentPosition, rawData, " ")
             currentPosition = firstSpace + 1
-            secondSpace = InStr(currentPosition, cccam, " ")
-            server = Mid(cccam, firstSpace + 1, (secondSpace - firstSpace - 1))
+            secondSpace = InStr(currentPosition, rawData, " ")
+            server = Mid(rawData, firstSpace + 1, (secondSpace - firstSpace - 1))
             currentPosition = secondSpace
-            firstSpace = InStr(currentPosition, cccam, " ")
+            firstSpace = InStr(currentPosition, rawData, " ")
             currentPosition = firstSpace + 1
-            secondSpace = InStr(currentPosition, cccam, " ")
-            port = Mid(cccam, firstSpace + 1, (secondSpace - firstSpace - 1))
+            secondSpace = InStr(currentPosition, rawData, " ")
+            port = Mid(rawData, firstSpace + 1, (secondSpace - firstSpace - 1))
             currentPosition = secondSpace
-            firstSpace = InStr(currentPosition, cccam, " ")
+            firstSpace = InStr(currentPosition, rawData, " ")
             currentPosition = firstSpace + 1
-            secondSpace = InStr(currentPosition, cccam, " ")
-            user = Mid(cccam, firstSpace + 1, (secondSpace - firstSpace - 1))
+            secondSpace = InStr(currentPosition, rawData, " ")
+            user = Mid(rawData, firstSpace + 1, (secondSpace - firstSpace - 1))
             currentPosition = secondSpace
-            firstSpace = InStr(currentPosition, cccam, " ")
+            firstSpace = InStr(currentPosition, rawData, " ")
             currentPosition = firstSpace + 1
-            secondSpace = InStr(currentPosition, cccam, "C:") - 2
-            password = Mid(cccam, firstSpace + 1, (secondSpace - firstSpace - 1))
+            secondSpace = InStr(currentPosition, rawData, "C:") - 2
+            password = Mid(rawData, firstSpace + 1, (secondSpace - firstSpace - 1))
             currentPosition = secondSpace
             listOfServers.Add(server)
             listOfServers.Add(port)
@@ -45,42 +39,35 @@ Public Class Form1
         Dim seqc As Process
         seqc = New Process()
         seqc = Process.Start("C:\seqc\seqc.exe")
-        System.Threading.Thread.Sleep(500) ' Wait 1 sec and hit enter on main screen
-        AppActivate(seqc.Id)
-
+        System.Threading.Thread.Sleep(500) ' Launch the app and wait 0.500 sec to load
+        AppActivate(seqc.Id) ' Set focus to the app
         My.Computer.Keyboard.SendKeys("~", True)
         System.Threading.Thread.Sleep(200)
         My.Computer.Keyboard.SendKeys("{TAB}", True)
         For index = 0 To 8
-
             My.Computer.Keyboard.SendKeys("~", True)
             System.Threading.Thread.Sleep(200)
-            My.Computer.Keyboard.SendKeys("{DOWN}", True)
-            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            My.Computer.Keyboard.SendKeys("{DOWN}{TAB}", True)
             System.Windows.Forms.Clipboard.SetText(listOfServers.ElementAt(index * 4 + 0))
-            My.Computer.Keyboard.SendKeys("^v", True)
-            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            My.Computer.Keyboard.SendKeys("^v{TAB}", True)
             System.Windows.Forms.Clipboard.SetText(listOfServers.ElementAt(index * 4 + 1))
-            My.Computer.Keyboard.SendKeys("^v", True)
-            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            My.Computer.Keyboard.SendKeys("^v{TAB}", True)
             System.Windows.Forms.Clipboard.SetText(listOfServers.ElementAt(index * 4 + 2))
-            My.Computer.Keyboard.SendKeys("^v", True)
-            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            My.Computer.Keyboard.SendKeys("^v{TAB}", True)
             System.Windows.Forms.Clipboard.SetText(listOfServers.ElementAt(index * 4 + 3))
-            My.Computer.Keyboard.SendKeys("^v", True)
-            My.Computer.Keyboard.SendKeys("{TAB}", True)
-            My.Computer.Keyboard.SendKeys("~", True)
+            My.Computer.Keyboard.SendKeys("^v{TAB}~", True)
             System.Threading.Thread.Sleep(500)
         Next
-        My.Computer.Keyboard.SendKeys("{TAB}", True)
-        My.Computer.Keyboard.SendKeys("{TAB}", True)
-        My.Computer.Keyboard.SendKeys("{TAB}", True)
-        My.Computer.Keyboard.SendKeys("~", True)
+        My.Computer.Keyboard.SendKeys("{TAB}{TAB}{TAB}~", True)
         System.Threading.Thread.Sleep(500)
         System.Windows.Forms.Clipboard.SetText(filename)
-        My.Computer.Keyboard.SendKeys("^v", True)
-        My.Computer.Keyboard.SendKeys("~", True)
+        My.Computer.Keyboard.SendKeys("^v~", True)
         seqc.Kill()
         Close()
-    End Sub
+
+        Private Function getItem(loop As interger)
+        If [loop] < 8 Then
+
+        End If
+        End Sub
 End Class
